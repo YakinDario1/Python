6e42dffff7324ba5ca9b30c2c94a55b1fd8d4817
Yakin Dario <yakin_d.m_15@hotmail.com>
2023-11-05 16:45:48 -0600

    add: Practica y proyecto 1

---
+++ Proyecto/Proyecto1/Ejercicio1.py
@@ -0,0 +1,18 @@
+print("Me encata estudiar Python")
+print ("Estudiar con \"Python Total\" es super divertido")
+
+print("**************************")
+
+print("yakin"+ " "+"Dario")
+print("Holi como estas?")
+print("Línea 1 \nLínea 2 \nLínea 3\n")
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+
+print("Barrra Normal:/\nBarra Invertida \\")
+
+print("**********************************")
+print("Tu nombre es: "+input("Dime tu nombre: "))
+print("Tu apellido es: "+input("Dime tu apellido: "))
+print(input("Escribe tu nombre: ") + input("Escribe tu apellido: "))
+print(input("¿Que estas estudiando? "))
+print(input("¿En qué país vives? "))
---
+++ Proyecto/Proyecto1/Proyecto1.py
@@ -0,0 +1,5 @@
+# Vas a crear un código en Python que le pida a tu amigo que responda dos preguntas que
+# requieran una sola palabra cada una y que luego le muestre en pantalla esas palabras
+# combinadas, para formar una marca creativa. 
+
+print("La cerveza se llama\n"+"\""+input("¿cual es el nombre de tu mama?") + input("¿cual es el color favorito?") +"\"")
